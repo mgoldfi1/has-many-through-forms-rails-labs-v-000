a3f086c00e3a873d221411a6b2c610fcc36e8270
mgoldfi1 <mgoldfi1@gmail.com>
2018-08-23 12:14:59 -0700

    Done.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,8 @@
+
 class CommentsController < ApplicationController
 
   def create
+  
     comment = Comment.create(comment_params)
     redirect_to comment.post
   end
@@ -8,6 +10,6 @@ class CommentsController < ApplicationController
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username])
+    params.require(:comment).permit(:content, :post_id, :user_id, user_attributes:[:username, :email])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  accepts_nested_attributes_for :categories, reject_if: proc {|attributes| Category.find_by(attributes)}
+
+
+
+
 
 
 end
